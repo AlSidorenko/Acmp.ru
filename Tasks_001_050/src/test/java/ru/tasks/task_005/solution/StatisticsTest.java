546ce66c113254fd5c9e363fbee29b412cda2f7a
AlSidorenko <alek.sidorenko1979@gmail.com>
2018-02-05 22:29:50 +0200

    Correct task Task 005

--- Tasks_001_050/src/test/java/ru/tasks/task_005/solution/StatisticsTest.java
+++ Tasks_001_050/src/test/java/ru/tasks/task_005/solution/StatisticsTest.java
@@ -3,6 +3,7 @@ package ru.tasks.task_005.solution;
 import org.junit.Test;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -19,8 +20,29 @@ import static org.hamcrest.core.Is.is;
 public class StatisticsTest {
 
     @Test
-    public void whenStatisticsScore() throws IOException {
+    public void whenStatisticsScoreFive() throws IOException {
         Statistics statistics = new Statistics();
+        FileWriter fwInput = new FileWriter("C:\\Users\\Александр\\OneDrive\\Документы\\IdeaProject\\" +
+                "Acmp.ru\\Tasks_001_050\\src\\main\\java\\ru\\tasks\\task_005\\solution\\input.txt");
+        fwInput.write("5\r");
+        fwInput.write("4 16 19 31 2");
+        fwInput.close();
+        boolean expected = statistics.init();
+        FileReader frOutput = new FileReader("C:\\Users\\Александр\\OneDrive\\Документы\\IdeaProject\\" +
+                "Acmp.ru\\Tasks_001_050\\src\\main\\java\\ru\\tasks\\task_005\\solution\\output.txt");
+        Scanner scanner = new Scanner(frOutput);
+        boolean output = scanner.hasNextLine();
+        assertThat(expected, is(output));
+    }
+
+    @Test
+    public void whenStatisticsScoreEight() throws IOException {
+        Statistics statistics = new Statistics();
+        FileWriter fwInput = new FileWriter("C:\\Users\\Александр\\OneDrive\\Документы\\IdeaProject\\" +
+                "Acmp.ru\\Tasks_001_050\\src\\main\\java\\ru\\tasks\\task_005\\solution\\input.txt");
+        fwInput.write("8\r");
+        fwInput.write("29 4 7 12 15 17 24 1");
+        fwInput.close();
         boolean expected = statistics.init();
         FileReader frOutput = new FileReader("C:\\Users\\Александр\\OneDrive\\Документы\\IdeaProject\\" +
                 "Acmp.ru\\Tasks_001_050\\src\\main\\java\\ru\\tasks\\task_005\\solution\\output.txt");
