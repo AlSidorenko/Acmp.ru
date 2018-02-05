39cca3db0acdfcb2409e2dac911191837966bd68
AlSidorenko <alek.sidorenko1979@gmail.com>
2018-02-05 22:17:42 +0200

    Correct task Task 004

--- Tasks_001_050/src/test/java/ru/tasks/task_004/solution/GameNumbTest.java
+++ Tasks_001_050/src/test/java/ru/tasks/task_004/solution/GameNumbTest.java
@@ -3,6 +3,7 @@ package ru.tasks.task_004.solution;
 import org.junit.Test;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -19,8 +20,42 @@ import static org.hamcrest.core.Is.is;
 public class GameNumbTest {
 
     @Test
-    public void whenTryGuessNumb() throws IOException {
+    public void whenTryGuessNumbFive() throws IOException {
         GameNumb gameNumb = new GameNumb();
+        FileWriter fwInput = new FileWriter("C:\\Users\\Александр\\OneDrive\\Документы\\IdeaProject\\Acmp.ru\\" +
+                "Tasks_001_050\\src\\main\\java\\ru\\tasks\\task_004\\solution\\input.txt");
+        fwInput.write("5");
+        fwInput.close();
+        String expected = gameNumb.init();
+        FileReader fr = new FileReader("C:\\Users\\Александр\\OneDrive\\Документы\\IdeaProject\\Acmp.ru\\" +
+                "Tasks_001_050\\src\\main\\java\\ru\\tasks\\task_004\\solution\\output.txt");
+        Scanner sc = new Scanner(fr);
+        String output = sc.next();
+        assertThat(expected, is(output));
+    }
+
+    @Test
+    public void whenTryGuessNumbTwo() throws IOException {
+        GameNumb gameNumb = new GameNumb();
+        FileWriter fwInput = new FileWriter("C:\\Users\\Александр\\OneDrive\\Документы\\IdeaProject\\Acmp.ru\\" +
+                "Tasks_001_050\\src\\main\\java\\ru\\tasks\\task_004\\solution\\input.txt");
+        fwInput.write("2");
+        fwInput.close();
+        String expected = gameNumb.init();
+        FileReader fr = new FileReader("C:\\Users\\Александр\\OneDrive\\Документы\\IdeaProject\\Acmp.ru\\" +
+                "Tasks_001_050\\src\\main\\java\\ru\\tasks\\task_004\\solution\\output.txt");
+        Scanner sc = new Scanner(fr);
+        String output = sc.next();
+        assertThat(expected, is(output));
+    }
+
+    @Test
+    public void whenTryGuessNumbSeven() throws IOException {
+        GameNumb gameNumb = new GameNumb();
+        FileWriter fwInput = new FileWriter("C:\\Users\\Александр\\OneDrive\\Документы\\IdeaProject\\Acmp.ru\\" +
+                "Tasks_001_050\\src\\main\\java\\ru\\tasks\\task_004\\solution\\input.txt");
+        fwInput.write("7");
+        fwInput.close();
         String expected = gameNumb.init();
         FileReader fr = new FileReader("C:\\Users\\Александр\\OneDrive\\Документы\\IdeaProject\\Acmp.ru\\" +
                 "Tasks_001_050\\src\\main\\java\\ru\\tasks\\task_004\\solution\\output.txt");
